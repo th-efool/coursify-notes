1947e5ea50bfbac3f6f91f56b3f876cca78d287a
Agrim Singh <asas05202415@gmail.com>
2026-03-08 06:41:32 +0530

    Drizzle setup

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,13 @@
+import {drizzle} from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import * as schema from '../../../migrations/schema';
+dotenv.config({path: `.env`});
+
+if(!process.env.DATABASE_URL){
+    console.error('MongoDB URI is missing');
+}
+
+const client = postgres(process.env.DATABASE_URL as string, {max:1});
+const db = drizzle(client, {schema});
+export default db;
